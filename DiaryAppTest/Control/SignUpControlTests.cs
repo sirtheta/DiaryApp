a993a4df71b28460bc1210f7434258aa43813204
sirtheta <neo38@bluewin.ch>
2021-03-12 21:46:48 +0100

    finished test methods for SignUpControl

--- DiaryAppTest/Control/SignUpControlTests.cs
+++ DiaryAppTest/Control/SignUpControlTests.cs
@@ -1,68 +1,106 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Security;
 
 namespace DiaryApp.Test
 {
-  [TestClass()]
+  [TestClass]
   public class SignUpControlTests
   {
+    //Is run once when test is started,
+    [AssemblyInitialize]
+    public static void Initialize(TestContext context)
+    {
+      //Drop existing DB
+      using (var deleteModel = new DiaryContext())
+      {
+        deleteModel.Database.Delete();
+      }
+      //Create user in empty DB
+      using var model = new DiaryContext();
+      var user1 = new UserModel { UserName = "Tester" };
+      var user2 = new UserModel { UserName = "Tester2" };
+      model.Users.Add(user1);
+      model.Users.Add(user2);
+      model.SaveChanges();
+    }
+
+    //is running after all test has finished
+    [AssemblyCleanup]
+    public static void Cleanup()
+    {
+      //Drop testing DB
+      using (var deleteModel = new DiaryContext())
+      {
+        deleteModel.Database.Delete();
+      }
+    }
 
     SignUpControl _signUpControl = new SignUpControl();
 
     [TestMethod("Password Regex Test true")]
     public void CheckPwdWithRegexTest()
     {      
-      _signUpControl.SignInPassword = ConvertToSecureString("5T!kq*o4f");
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5T!kq*o4f");
       Assert.IsTrue(_signUpControl.CheckPwdWithRegex());
     }
 
     [TestMethod("Password Regex Test: no special char")]
-    public void CheckPwdWithRegexTestFalse()
+    public void CheckPwdWithRegexTestFalseNoSpecial()
     {
-      _signUpControl.SignInPassword = ConvertToSecureString("5Tkqo4f89");
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5Tkqo4f89");
       Assert.IsFalse(_signUpControl.CheckPwdWithRegex());
     }
 
     [TestMethod("Password Regex Test: not enough char")]
-    public void CheckPwdWithRegexTestFalseNSpecial()
+    public void CheckPwdWithRegexTestFalseNotEnough()
     {
-      _signUpControl.SignInPassword = ConvertToSecureString("5Tkqo4*");
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5Tkqo4*");
       Assert.IsFalse(_signUpControl.CheckPwdWithRegex());
     }
 
     [TestMethod("Password Regex Test: no lower char")]
-    public void CheckPwdWithRegexTestFalseNLower()
+    public void CheckPwdWithRegexTestFalseNoLower()
     {
-      _signUpControl.SignInPassword = ConvertToSecureString("5TKOP4*9");
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5TKOP4*9");
+      Assert.IsFalse(_signUpControl.CheckPwdWithRegex());
+    }
+
+    [TestMethod("Password Regex Test: no upper char")]
+    public void CheckPwdWithRegexTestFalseNoUpper()
+    {
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5tkop4*9");
+      Assert.IsFalse(_signUpControl.CheckPwdWithRegex());
+    }
+
+    [TestMethod("Password Regex Test: no numbers")]
+    public void CheckPwdWithRegexTestFalseNoNumbers()
+    {
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("TtkopI*p");
       Assert.IsFalse(_signUpControl.CheckPwdWithRegex());
     }
 
     [TestMethod("Password Match Test: true")]   
     public void CheckPasswordMatchTestTrue()
     {
-      _signUpControl.SignInPassword = ConvertToSecureString("5T!kq*o4f");
-      _signUpControl.SignInPasswordConfirm = ConvertToSecureString("5T!kq*o4f");
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5T!kq*o4f");
+      _signUpControl.SignInPasswordConfirm = Helper.ConvertToSecureString("5T!kq*o4f");
       Assert.IsTrue(_signUpControl.CheckPasswordMatch());
     }
 
     [TestMethod("Password Match Test: false")]
     public void CheckPasswordMatchTestFalse()
     {
-      _signUpControl.SignInPassword = ConvertToSecureString("5T!kq*o4f");
-      _signUpControl.SignInPasswordConfirm = ConvertToSecureString("5T!kqo*4f");
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5T!kq*o4f");
+      _signUpControl.SignInPasswordConfirm = Helper.ConvertToSecureString("5T!kqo*4f");
       Assert.IsFalse(_signUpControl.CheckPasswordMatch());
     }
 
-    //TODO, Test with DB.
     [TestMethod("User Duplicate: new user")]
     public void CheckUserDuplicateTest()
     {
-      PrepareData();
       _signUpControl.UserName = "gibberish";
       Assert.IsTrue(_signUpControl.CheckUserDuplicate());
     }
@@ -70,36 +108,23 @@ namespace DiaryApp.Test
     [TestMethod("User Duplicate: existing user")]
     public void CheckUserDuplicateTestFalse()
     {
-      PrepareData();
       _signUpControl.UserName = "Tester";
       Assert.IsFalse(_signUpControl.CheckUserDuplicate());
     }
 
-    private SecureString ConvertToSecureString(string password)
+    [TestMethod("Create new User")]
+    public void TestCreateNewUser()
     {
-      if (password == null)
-        throw new ArgumentNullException("password");
-
-      var securePassword = new SecureString();
-
-      foreach (char c in password)
-        securePassword.AppendChar(c);
-
-      securePassword.MakeReadOnly();
-      return securePassword;
+      _signUpControl.FirstName = "John";
+      _signUpControl.LastName = "Doe";
+      _signUpControl.UserName = "JDO";
+      _signUpControl.SignInPassword = Helper.ConvertToSecureString("5T!kq*o4f");
+      _signUpControl.CreateNewUser();
+      var user = DbController.GetUserFromDb("JDO").SingleOrDefault();
+      Assert.AreEqual("John", user.FirstName);
+      Assert.AreEqual("Doe", user.LastName);
+      Assert.AreEqual("JDO", user.UserName);
+      Assert.IsTrue(SecurePasswordHasher.Verify("5T!kq*o4f", user.Password));
     }
-
-    private void PrepareData()
-    {
-      using (var model = new DiaryContext())
-      {
-        var user1 = new UserModel { UserName = "Tester" };
-        var user2 = new UserModel { UserName = "Tester2" };
-        model.Users.Add(user1);
-        model.Users.Add(user2);
-        model.SaveChanges();
-      }
-    }
-
   }
 }
---
+++ DiaryAppTest/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace DiaryApp.Test
+{
+  internal static class Helper
+  {
+    public static SecureString ConvertToSecureString(string password)
+    {
+      if (password == null)
+        throw new ArgumentNullException("password");
+
+      var securePassword = new SecureString();
+
+      foreach (char c in password)
+        securePassword.AppendChar(c);
+
+      securePassword.MakeReadOnly();
+      return securePassword;
+    }
+  }
+}
