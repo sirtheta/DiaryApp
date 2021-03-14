25879e35e64aae2ec95edbe3319b6b31b46ee956
sirtheta <neo38@bluewin.ch>
2021-03-14 19:54:24 +0100

    cleanup

--- DiaryApp/Utils/DatabaseInitializer.cs
+++ DiaryApp/Utils/DatabaseInitializer.cs
@@ -30,32 +30,38 @@ namespace DiaryApp
       if (!db.DiaryEntrys.Any())
       {
         byte[] img;
-        string testText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+        string testText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor " +
+          "invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
+          "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. " +
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et " +
+          "dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet " +
+          "clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
         using (WebClient webClient = new WebClient())
         {
           img = webClient.DownloadData("https://onaliternote.files.wordpress.com/2016/11/wp-1480230666843.jpg");
         }
+
         List<DiaryEntryModel> lstTestEntrys = new List<DiaryEntryModel>()
-          { new DiaryEntryModel
-            {
-              UserId = 1,
-              Text = testText,
-              TagBirthday = true,
-              TagFriends = true,
-              ByteImage = img,
-              Date = DateTime.Today
-            }
-          };
-        lstTestEntrys.Add(new DiaryEntryModel
-        {
-          UserId = 2,
-          Text = "this is user Id 2 and not visible with userId 1.",
-          TagFriends = true,
-          TagBirthday = true,
-          TagFamily = true,
-          ByteImage = img,
-          Date = DateTime.Today.AddDays(-6)
-        });
+        { new DiaryEntryModel
+          {
+            UserId = 1,
+            Text = testText,
+            TagBirthday = true,
+            TagFriends = true,
+            ByteImage = img,
+            Date = DateTime.Today
+          },
+          new DiaryEntryModel
+          {
+            UserId = 2,
+            Text = "this is user Id 2 and not visible with userId 1.",
+            TagFriends = true,
+            TagBirthday = true,
+            TagFamily = true,
+            ByteImage = img,
+            Date = DateTime.Today.AddDays(-6)
+          }
+        };
 
         //more entrys
         for (int i = 6; i < 50; i++)
